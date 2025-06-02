0b008e3608774da95800e0467eed8f76c6064bec
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-06-02 17:41:34 -0600

    Config the image picker

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -1,22 +1,52 @@
-import { launchCameraAsync } from "expo-image-picker";
-import { Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  PermissionStatus,
+  useCameraPermissions,
+} from "expo-image-picker";
+import { Alert, Button, View } from "react-native";
 
-function ImagePicker () {
-    async function takeImageHandler() {
-        const image = await launchCameraAsync({
-            allowsEditing: true,
-            aspect: [16, 9],
-            quality: 0.5,
-        });
-        console.log(image);
+function ImagePicker() {
+  const [cameraPermissionsInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermission() {
+    if (cameraPermissionsInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
     }
 
-    return (
-        <View>
-            <View></View>
-            <Button title="Take image"  onPress={takeImageHandler}/>
-        </View>
-    )
+    if (cameraPermissionsInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions to use this app."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  async function takeImageHandler() {
+    const hasPermission = await verifyPermission();
+    
+    if(!hasPermission){
+        return;
+    }
+    
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    console.log(image);
+  }
+
+  return (
+    <View>
+      <View></View>
+      <Button title="Take image" onPress={takeImageHandler} />
+    </View>
+  );
 }
 
 export default ImagePicker;
