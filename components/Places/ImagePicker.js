e3ced15e833be89da30861b0b42e431d87fdb7cb
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-06-02 18:02:21 -0600

    Create the preview of the image

--- components/Places/ImagePicker.js
+++ components/Places/ImagePicker.js
@@ -3,11 +3,13 @@ import {
   PermissionStatus,
   useCameraPermissions,
 } from "expo-image-picker";
-import { Alert, Button, View } from "react-native";
+import { useState } from "react";
+import { Alert, Button, Image, StyleSheet, View, Text } from "react-native";
+import { Colors } from "../../constants/colors";
 
 function ImagePicker() {
-  const [cameraPermissionsInformation, requestPermission] =
-    useCameraPermissions();
+  const [pickedImage, setPickedImage] = useState("");
+  const [cameraPermissionsInformation, requestPermission] = useCameraPermissions();
 
   async function verifyPermission() {
     if (cameraPermissionsInformation.status === PermissionStatus.UNDETERMINED) {
@@ -28,25 +30,50 @@ function ImagePicker() {
 
   async function takeImageHandler() {
     const hasPermission = await verifyPermission();
-    
-    if(!hasPermission){
-        return;
+
+    if (!hasPermission) {
+      return;
     }
-    
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+
+    if (!image.cancelled && image.assets?.[0]?.uri) {
+      setPickedImage(image.assets[0].uri);
+    }
+  }
+
+  let imagePreview = <Text>No image taken yet!</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
   }
 
   return (
     <View>
-      <View></View>
+      <View style={styles.imagePreview}>{imagePreview}</View>
       <Button title="Take image" onPress={takeImageHandler} />
     </View>
   );
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
