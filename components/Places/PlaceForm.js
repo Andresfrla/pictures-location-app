f3bf80bb69c5ac00d99cca5ee1bc69780c671121
Andres Francisco Franco <125943749+Andresfrla@users.noreply.github.com>
2025-06-05 18:28:21 -0600

    Create a function for passing the data trough props

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -4,8 +4,9 @@ import { Colors } from "../../constants/colors";
 import ImagePicker from "./ImagePicker";
 import LocationPicker from "./LocationPicker";
 import Button from "../UI/Button";
+import { Place } from "../../models/place";
 
-function PlaceForm() {
+function PlaceForm({onCreatePlace}) {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [selectedImage, setSelectedImage] = useState();
   const [pickedLocation, setPickedLocation] = useState();
@@ -23,8 +24,9 @@ function PlaceForm() {
   }, []);
 
   function savePlaceHandler() {
-    
-  } 
+    const placeData = new Place(enteredTitle, selectedImage, pickedLocation);
+    onCreatePlace(placeData);
+  }
 
   return (
     <ScrollView style={styles.form}>
